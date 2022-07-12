5e1fd48e9819b142225b3c5b8d2f8e5372aa97ef
sunil <sunil@dzangolab.com>
2022-07-12 19:58:08 +0545

    fix: deploy file

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,10 @@ yarn run build
 cd dist
 
 git init
-git checkout -b master
+git checkout master
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:scherbatsky-jr/scherbatsky=jr.github.io.git master
+git push -f git@github.com:scherbatsky-jr/scherbatsky-jr.github.io.git master
 
 cd -
