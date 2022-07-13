58e3df17f5adc920dae0524d1aad7810392107e9
sunil <sunil@dzangolab.com>
2022-07-13 20:55:49 +0545

    Updates

---
+++ component---src-pages-index-js-feebd9b6dd533b55741c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkscherbatsky=self.webpackChunkscherbatsky||[]).push([[678],{6558:function(e,n,s){s.r(n);var t=s(7294);n.default=function(){return t.createElement("main",null,"Hello my name is sunil")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-feebd9b6dd533b55741c.js.map
